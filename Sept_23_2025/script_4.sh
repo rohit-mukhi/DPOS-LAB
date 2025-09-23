9d9735a8d0168b23600bd428fc4ee7b73acd3f31
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-23 08:56:22 +0530

    Create script_4.sh

---
+++ Sept_23_2025/script_4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is used to take user input from arguments and print the value of the argumenmts
+
+name="$1"
+
+job="$2"
+
+echo "Hello $name"
+echo "I hope working as a $job is interesting"
