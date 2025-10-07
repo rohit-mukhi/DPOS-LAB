b7b5051c8ee82bc957d27701d4cb3111f0011650
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-07 09:24:15 +0530

    Create Question_2.sh

---
+++ Octo_7_2025/Question_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Name: "
+whoami
+echo ""
+echo "Unix system being used: "
+uname -a
+echo ""
+echo "Shell: "
+echo $SHELL
+echo""
+echo "Your current working directory: "
+pwd
+echo ""
+echo "List of files in the directory: "
+ls -l
+
