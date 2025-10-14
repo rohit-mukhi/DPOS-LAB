6ed8000b073f29c9ade5e95d321eb155743d7fb7
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-14 09:08:08 +0530

    Create Question_1.sh

---
+++ Oct_14_2025/Practice_scripts/Question_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script teaches conditionals and floating point calculations with awk utility
+
+echo "Enter radius: "
+read radius
+area=0
+if [ $radius -le 0  ]; then
+	echo "Radius cannot be negative"
+else
+	area=$(awk "BEGIN {printf \"%.2f\\n\", 3.14 *  $radius * $radius}")
+	echo "Area of circle is $area"
+fi
