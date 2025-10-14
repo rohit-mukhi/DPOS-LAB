2bbcb7d829537fc496c0daaf48f24b2ac2643e3d
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-14 09:26:42 +0530

    Update Question_1.sh

--- Oct_14_2025/Practice_scripts/Question_1.sh
+++ Oct_14_2025/Practice_scripts/Question_1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script teaches conditionals and floating point calculations with awk utility
+# The awk is a good for text formatting. For high level precision use bc
 
 echo "Enter radius: "
 read radius
