3b3969fa33ce77afefc9f47c773d844356efd829
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-12-15 19:09:07 +0530

    Create Question_1_d.c

---
+++ Assignment_4/Question_1_d.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+    pid = fork();
+    int status;
+    if(pid == 0) {
+        printf("Child with pid: %d task starts\n", getpid());
+        printf("Child with pid: %d task stops\n", getpid());
+    } else {
+        printf("Parent with pid: %d starts waiting for child process\n", getpid());
+        wait(NULL);
+        printf("Parent with pid: %d overtakes control from child\n", getpid());
+    }
+    
+    return 0;
+}
